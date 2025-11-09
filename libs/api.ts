3fd9fa88b69e1797443a56d6ed59d9cc2ee75b16
hamzDevv <hamzjsc@gmail.com>
2025-11-09 11:33:19 +0700

    Tauk Ahk

--- libs/api.ts
+++ libs/api.ts
@@ -1,4 +1,4 @@
-export const apiFetch = async route => {
+export const apiFetch = async (route: string) => {
     try {
         const jikanApi = `${process.env.NEXT_PUBLIC_API_BASE_URL}/${route}`;
         const res = await fetch(jikanApi)
